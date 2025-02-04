ed9bac952d79b2531ba99d6becdc63e003615ed9
Martyn Garcia <martyn.garcia@gmail.com>
2025-02-04 15:30:11 -0700

    test buttons

--- bitte-agent/src/app/page.tsx
+++ bitte-agent/src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>Test flow</h1>
+      <h2>NEAR Intents</h2>
         <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
           register
         </button>
